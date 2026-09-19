89eeea762a73f7a3102f3b2459d64425caed8ffe
DanMurphyy <highquality1996@gmail.com>
2026-09-19 12:57:59 +0500

    moved

--- app/src/main/java/com/danmurphyy/bilimcha/features/numbers/Number.kt
+++
@@ -1,9 +0,0 @@
-package com.danmurphyy.bilimcha.features.numbers
-
-data class Number(
-    val value: Int,
-    val nameEn: String,
-    val nameRu: String,
-    val audioEn: String,
-    val audioRu: String,
-)
